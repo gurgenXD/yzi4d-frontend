f2f3ea2b4fd2ac82316cf92e472129011316fa89
AsuraBot <homecat13@gmail.com>
2023-11-15 21:50:49 +0300

    Fixed YM

--- src/app/components/YandexMetrics.tsx
+++ src/app/components/YandexMetrics.tsx
@@ -4,6 +4,7 @@ export default function YandexMetrics() {
     return (
         <>
             <Script
+                id="YandexMetricsYZI4D"
                 dangerouslySetInnerHTML={{
                     __html: `
                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
